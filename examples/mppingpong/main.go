f6cc27b973b7e2aafd168883675bad5155790e98
nicolaswith <nicolas.with@web.de>
2021-09-21 15:17:30 +0200

    first working example

--- examples/mppingpong/main.go
+++ examples/mppingpong/main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"bytes"
 	"encoding/gob"
 	"flag"
+	"fmt"
 	smp "github.com/netsys-lab/scion-path-discovery/api"
 	"github.com/netsys-lab/scion-path-discovery/packets"
 	"github.com/netsys-lab/scion-path-discovery/pathselection"
 	"github.com/scionproto/scion/go/lib/snet"
-	"github.com/scionproto/scion/go/lib/snet/path"
 	log "github.com/sirupsen/logrus"
-	"strconv"
+	"strings"
 	"time"
 )
+
+type PathPacket struct {
+	Path string
+}
 
 //LastSelection users could add more fields
 type LastSelection struct {
@@ -56,12 +61,94 @@ func setLoging() {
 	}
 }
 
+func PathToString(path snet.Path) string {
+	if path == nil {
+		return ""
+	}
+	intfs := path.Metadata().Interfaces
+	if len(intfs) == 0 {
+		return ""
+	}
+	var hops []string
+	intf := intfs[0]
+	hops = append(hops, fmt.Sprintf("%s %s",
+		intf.IA,
+		intf.ID,
+	))
+	for i := 1; i < len(intfs)-1; i += 2 {
+		inIntf := intfs[i]
+		outIntf := intfs[i+1]
+		hops = append(hops, fmt.Sprintf("%s %s %s",
+			inIntf.ID,
+			inIntf.IA,
+			outIntf.ID,
+		))
+	}
+	intf = intfs[len(intfs)-1]
+	hops = append(hops, fmt.Sprintf("%s %s",
+		intf.ID,
+		intf.IA,
+	))
+	return fmt.Sprintf("[%s]", strings.Join(hops, ">"))
+}
+
+func receivePackets(mpSock *smp.MPPeerSock) {
+	go func() {
+		//search for incoming connection. There is only one (until now).
+		for _, conn := range mpSock.UnderlaySocket.GetConnections() {
+			//stay at incoming connection.
+			for {
+				if conn.GetType() == packets.ConnectionTypes.Incoming {
+					bts := make([]byte, packets.PACKET_SIZE)
+					n, err := conn.Read(bts)
+					if err != nil {
+						log.Fatalf("Failed to read bytes from peer %s, err: %v", *remoteAddr, err)
+					}
+
+					pkt := PathPacket{}
+					network := bytes.NewBuffer(bts)
+					dec := gob.NewDecoder(network)
+					err = dec.Decode(&pkt)
+
+					log.Debugf("Read %d bytes from %s", n, *remoteAddr)
+					log.Infof("Ping from %s over %s", *remoteAddr, pkt.Path)
+				}
+			}
+		}
+	}()
+}
+
+func sendPackets(mpSock *smp.MPPeerSock) {
+	go func() {
+		for {
+			for _, conn := range mpSock.UnderlaySocket.GetConnections() {
+				if conn.GetType() == packets.ConnectionTypes.Outgoing {
+					str := PathToString((*conn.GetPath()).Copy())
+
+					var network bytes.Buffer
+					enc := gob.NewEncoder(&network)
+					pkt := PathPacket{
+						Path: str,
+					}
+					err := enc.Encode(&pkt)
+					n, err := conn.Write(network.Bytes())
+
+					if err != nil {
+						log.Fatalf("Failed to write bytes from peer %s, err: %v", *remoteAddr, err)
+					}
+					log.Debugf("Wrote %d bytes to %s", n, *remoteAddr)
+					log.Infof("Ping to %s over %s", *remoteAddr, str)
+				}
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}()
+}
+
 func main() {
-	// peers := []string{"peer1", "peer2", "peer3"} // Later real addresses
 	flag.Parse()
 	setLoging()
-	log.SetLevel(log.DebugLevel)
-	gob.Register(path.Path{})
+	gob.Register(PathPacket{})
 	lastSelection := LastSelection{}
 
 	mpSock := smp.NewMPPeerSock(*localAddr, nil)
@@ -89,85 +176,28 @@ func main() {
 			log.Fatal("Failed to connect in-dialing peer", err)
 		}
 		log.Infof("Connected to %s", remote.String())
-		for {
-			for _, conn := range mpSock.UnderlaySocket.GetConnections() {
-				if conn.GetType() == packets.ConnectionTypes.Incoming {
-					bts := make([]byte, packets.PACKET_SIZE)
-					//n, err := mpSock.Read(bts)
-					n, err := conn.Read(bts)
-					if err != nil {
-						log.Fatalf("Failed to read bytes from peer %s, err: %v", remote.String(), err)
-					}
-					connNum := string(bts[:n])
 
-
-					if n > 1 {continue}
-
-
-					log.Debugf("Read %d bytes from %s (origin %s)", n, remote.String(), connNum)
-					log.Infof("Ping from %s", remote.String())
-					n, err = mpSock.Write(bts)
-					//n, err = conn.Write(bts)
-					if err != nil {
-						log.Fatalf("Failed to write bytes from peer %s, err: %v", remote.String(), err)
-					}
-					log.Debugf("Wrote %d bytes to %s", n, remote.String())
-					log.Infof("Pong to %s", remote.String())
-				}
-				//time.Sleep(1 * time.Second)
-			}
-		}
-
-
-
-
-
-
-
+		sendPackets(mpSock)
+		receivePackets(mpSock)
 	} else {
 		peerAddr, err := snet.ParseUDPAddr(*remoteAddr)
 		if err != nil {
 			log.Fatalf("Failed to parse remote addr %s, err: %v", *remoteAddr, err)
 		}
 		mpSock.SetPeer(peerAddr)
-
 		err = mpSock.Connect(&lastSelection, nil)
 		log.Infof("Connected to %s", *remoteAddr)
 		if err != nil {
 			log.Fatal("Failed to connect MPPeerSock", err)
 		}
 
-		for {
-			for i, conn := range mpSock.UnderlaySocket.GetConnections() {
-				if conn.GetType() == packets.ConnectionTypes.Outgoing {
-					bts := make([]byte, packets.PACKET_SIZE)
-					//n, err := mpSock.Write([]byte(strconv.Itoa(i)))
-					n, err := conn.Write([]byte(strconv.Itoa(i)))
-					if err != nil {
-						log.Fatalf("Failed to write bytes from peer %s, err: %v", *remoteAddr, err)
-					}
-					log.Debugf("Wrote %d bytes to %s over conn %d", n, *remoteAddr, i)
-					log.Infof("Ping to %s", *remoteAddr)
-					n, err = mpSock.Read(bts)
-					//n, err = conn.Read(bts)
-					if err != nil {
-						log.Fatalf("Failed to read bytes from peer %s, err: %v", *remoteAddr, err)
-					}
-					connNum, err := strconv.Atoi(string(bts[:1]))
-					println(i, string(bts[:n]), connNum)
-					if err != nil || connNum != i {
-						log.Infof("Pong from wrong connection %d", connNum)
-					}
-					log.Debugf("Read %d bytes from %s", n, *remoteAddr)
-					log.Infof("Pong from %s", *remoteAddr)
-				}
-				time.Sleep(1 * time.Second)
-			}
-		}
+		sendPackets(mpSock)
+		receivePackets(mpSock)
 	}
+
+	select {}
 
 	// mpSock.SetPeer(remote)
 	// mpSock.Connect(customPathSelectAlg)
 	//defer mpSock.Disconnect()
-
 }
