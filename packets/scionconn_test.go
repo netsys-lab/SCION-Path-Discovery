7c63b89d934c4499a2819788f7f3d11711ea6cc6
Marten Gartner <martenwallewein@web.de>
2021-10-19 13:29:50 +0200

    Add testing workflow and first testcase

---
+++ packets/scionconn_test.go
@@ -0,0 +1,46 @@
+package packets
+
+import (
+	"testing"
+
+	"github.com/netsec-ethz/scion-apps/pkg/appnet"
+)
+
+func Test_SCIONConn_Listen(t *testing.T) {
+	t.Run("SCIONConn Listen", func(t *testing.T) {
+		conn := SCIONTransportConstructor()
+		addr, err := appnet.ResolveUDPAddr("1-ff00:0:110,[127.0.0.12]:41000")
+		if err != nil {
+			t.Error(err)
+		}
+		err = conn.Listen(*addr)
+		if err != nil {
+			t.Error(err)
+		}
+		conn.Close()
+	})
+
+	t.Run("SCIONConn Dial", func(t *testing.T) {
+		conn := SCIONTransportConstructor()
+		addr, err := appnet.ResolveUDPAddr("1-ff00:0:110,[127.0.0.1]:42000")
+		if err != nil {
+			t.Error(err)
+		}
+		err = appnet.SetDefaultPath(addr)
+		if err != nil {
+			t.Error(err)
+		}
+		p, err := addr.GetPath()
+		if err != nil {
+			t.Error(err)
+		}
+		err = conn.Dial(*addr, &p)
+		if err != nil {
+			t.Error(err)
+		}
+
+		defer conn.Close()
+
+	})
+
+}
